6e849107b04966f81a3d2b4c77241236b497b3f9
Zain Malik <zainmalik7345@gmail.com>
2024-06-27 22:30:31 +0500

    FIXES: remove extra engine args (causing issue for Postgresql)

--- app/database.py
+++ app/database.py
@@ -6,9 +6,8 @@ from app.settings import DbCreds
 
 SQLALCHEMY_DATABASE_URL = DbCreds().database_url
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
