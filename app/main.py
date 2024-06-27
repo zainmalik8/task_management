ab6cddbf8fd3b79e2551277a36ae8fd562dab68c
Zain Malik <zainmalik7345@gmail.com>
2024-06-27 23:23:11 +0500

    FEAT: API V1 app

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 
+from .routers import user
+
 app = FastAPI()
+
+app_v1 = FastAPI()
+
+app_v1.include_router(user.router_v1)
 
 
 @app.get('/')
 def root():
     return "Task management service."
+
+
+app.mount("/api/v1", app_v1)
