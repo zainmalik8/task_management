161b97ebc06ba28275d236ec9577ea0115c1fb1a
Zain Malik <zainmalik7345@gmail.com>
2024-06-28 04:42:29 +0500

    FEAT: JWT & Profile router

--- app/main.py
+++ app/main.py
@@ -1,12 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 
-from .routers import user
+from .auth_dependency import JWTBearer
+from .dependencies import get_db
+from .routers import user, profile
+from .settings import JwtCreds
 
 app = FastAPI()
 
-app_v1 = FastAPI()
+app_v1 = FastAPI(dependencies=[Depends(get_db)])
 
 app_v1.include_router(user.router_v1)
+app_v1.include_router(profile.router_v1, dependencies=[Depends(JWTBearer(**JwtCreds.get_creds()))])
 
 
 @app.get('/')
