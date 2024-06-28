3884634c8f34b1a72a65162be14a53544462c327
Zain Malik <zainmalik7345@gmail.com>
2024-06-28 05:00:30 +0500

    FEAT: Alembic migration -> project

--- alembic/env.py
+++ alembic/env.py
@@ -8,6 +8,7 @@ from alembic import context
 from app.settings import DbCreds
 from app.models.base import Base
 from app.models.user import User  # noqa
+from app.models.project import Project  # noqa
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
