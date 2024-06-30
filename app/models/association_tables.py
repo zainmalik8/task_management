999be951fe2583291eeb6b17da9bf87d21197e1d
Zain Malik <zainmalik7345@gmail.com>
2024-07-01 03:35:21 +0500

    UPDATE: Association table -> unique constraint

--- app/models/association_tables.py
+++ app/models/association_tables.py
@@ -1,10 +1,11 @@
-from sqlalchemy import Table, Column, ForeignKey
+from sqlalchemy import Table, Column, ForeignKey, UniqueConstraint
 
 from .base import Base
 
 project_users = Table(
     "project_users",
     Base.metadata,
-    Column('project_id', ForeignKey('projects.id')),
-    Column('user_id', ForeignKey('users.id')),
+    Column('project_id', ForeignKey('projects.id'), primary_key=True),
+    Column('user_id', ForeignKey('users.id'), primary_key=True),
+    UniqueConstraint('project_id', 'user_id', name='uq_project_user')
 )
