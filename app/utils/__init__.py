fd860881d15922858ed53b4d7a9002fd2cf11ec3
Zain Malik <zainmalik7345@gmail.com>
2024-07-01 01:59:52 +0500

    UPDATE: Utils -> enums

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,3 +1,3 @@
-from .enums import RoleEnum
+from .enums import RoleEnum, StatusEnum
 from .exceptions import BadRequestException
 from .functions import hash_password, verify_password, generate_access_token
--- app/utils/enums.py
+++ app/utils/enums.py
@@ -4,3 +4,9 @@ from enum import Enum
 class RoleEnum(str, Enum):
     user = "user"
     admin = "admin"
+
+
+class StatusEnum(str, Enum):
+    to_do = "to_do"
+    in_progress = "in_progress"
+    completed = "completed"
