5d139644f8e78e73aa9722a8a54df1ba632e7cd3
Zain Malik <zainmalik7345@gmail.com>
2024-07-01 03:36:33 +0500

    UPDATE: main file (Entry point) -> Include routers

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Depends
 
 from .auth_dependency import JWTBearer
 from .dependencies import get_db
-from .routers import user, profile, project, user_managment
+from .routers import user, profile, project, user_managment, task, comment
 from .settings import JwtCreds
 
 app = FastAPI()
@@ -11,7 +11,7 @@ app_v1 = FastAPI(dependencies=[Depends(get_db)])
 
 app_v1.include_router(user.router_v1)
 
-for router in [profile.router_v1, project.router_v1, user_managment.router_v1]:
+for router in [profile.router_v1, project.router_v1, user_managment.router_v1, task.router_v1, comment.router_v1]:
     app_v1.include_router(router, dependencies=[Depends(JWTBearer(**JwtCreds.get_creds()))])
 
 
